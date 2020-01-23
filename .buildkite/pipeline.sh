fde811657537df86d50e802c626baf0ee1d08e2b
Chris Deacy <deacy@uber.com>
2020-01-23 13:46:26 -0800

    test

--- .buildkite/pipeline.sh
+++ .buildkite/pipeline.sh
@@ -11,11 +11,13 @@ function generate_pipeline() {
     project_name=$(basename "$project");
 
     for job_type in $job_types ; do (
-      echo "  - label: '$project_name'";
+      echo "  - label: '$job_type'";
       echo "    commands:";
       echo "    - 'echo $project_name ${job_type}!'";
       echo "    timeout_in_minutes: 15";
-      echo "    parallelism: 5";
+      # echo "    parallelism: 5";
+      echo "    concurrency: 5";
+      echo "    concurrency_group: '$project_name'";
       echo "    agents:";
       echo "      queue: web-code-default";
     ); done;
