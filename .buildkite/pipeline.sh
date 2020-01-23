0918c0e9b1c5ad5e6c98dab1da6420ce20dd1d36
Chris Deacy <deacy@uber.com>
2020-01-23 13:56:23 -0800

    update pipeline

--- .buildkite/pipeline.sh
+++ .buildkite/pipeline.sh
@@ -1,26 +1,16 @@
 function generate_pipeline() {
-  local job_types="bundle-size cover flow integration lint"
-
-  for diff_id in $DIFF_IDS; do
-    echo $diff_id
-  done
-
   echo "steps:"
 
   for project in $PROJECTS ; do (
     project_name=$(basename "$project");
 
-    for job_type in $job_types ; do (
-      echo "  - label: '$job_type'";
-      echo "    commands:";
-      echo "    - 'echo $project_name ${job_type}!'";
-      echo "    timeout_in_minutes: 15";
-      # echo "    parallelism: 5";
-      echo "    concurrency: 5";
-      echo "    concurrency_group: '$project_name'";
-      echo "    agents:";
-      echo "      queue: web-code-default";
-    ); done;
+    echo "  - label: '$project_name %n'";
+    echo "    commands:";
+    echo "    - 'echo $project_name $$BUILDKITE_PARALLEL_JOB'";
+    echo "    timeout_in_minutes: 15";
+    echo "    parallelism: 5";
+    echo "    agents:";
+    echo "      queue: web-code-default";
   ); done;
 }
 
