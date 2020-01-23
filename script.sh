a394c16c51d64725fab51220b6a127b98ee98a77
Albert Wu <wua@uber.com>
2020-01-23 13:35:33 -0800

    debug

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -eo pipefail
+set -ex
 
 . utils.sh
 
@@ -9,10 +9,17 @@ cat ~/.docker/config.json
 IMAGE=027047743804.dkr.ecr.us-west-1.amazonaws.com/web-code:jenkins-plan-web-monorepo-9786
 PACKAGE=projects/example-trips-viewer-fusion
 
+echo "---"
+pwd
+echo "---"
+
 # volume-mount a yarn cache dir, to prevent excessive uNPM calls
 util_install_yarn_cache_tarball $PACKAGE
 
-docker run -i --rm $IMAGE bash <<CMD
+# get the yarn cache directory inside the docker container
+YARN_CACHE_DIR=$(docker run --rm "$IMAGE" bash -c 'yarn cache dir')
+
+docker run -v .yarn_cache:$YARN_CACHE_DIR -i --rm $IMAGE bash <<CMD
   jazelle ci --cwd $PACKAGE
   jazelle lint --cwd $PACKAGE
 CMD
