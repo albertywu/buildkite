a3db2da5ba017f3be2c62024e26cf13df8a39bda
Chris Deacy <chrisdothtml@gmail.com>
2020-01-23 12:56:04 -0800

    Update pipeline.sh

--- .buildkite/pipeline.sh
+++ .buildkite/pipeline.sh
@@ -1,1 +1,24 @@
-cat .buildkite/pipeline.yml | sed "s/\$BUILDKITE_AGENT_META_DATA_QUEUE/${BUILDKITE_AGENT_META_DATA_QUEUE:-default}/" | buildkite-agent pipeline upload
+function generate_pipeline() {
+  local job_types="bundle-size cover flow integration lint"
+
+  for diff_id in $DIFF_IDS; do
+    echo $diff_id
+  done
+
+  echo "steps:"
+
+  for project in $PROJECTS ; do (
+    project_name=$(basename "$project");
+
+    for job_type in $job_types ; do (
+      echo "  - label: '$project_name: $job_type'";
+      echo "    commands:";
+      echo "    - 'echo ${job_type}!'";
+      echo "    timeout_in_minutes: 15";
+      echo "    agents:";
+      echo "      queue: web-code-default";
+    ); done;
+  ); done;
+}
+
+generate_pipeline | buildkite-agent pipeline upload
