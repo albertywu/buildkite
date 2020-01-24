ae2fc69b579133e8d533ed2e56656c12d20f4d5b
Albert Wu <wua@uber.com>
2020-01-23 17:08:50 -0800

    fix

--- lint.sh
+++ lint.sh
@@ -14,7 +14,7 @@ echo "---"
 util_install_yarn_cache_tarball $PROJECT
 
 # get the yarn cache directory inside the docker container
-YARN_CACHE_DIR=$(docker run --rm "$IMAGE" bash -c 'yarn cache dir')
+YARN_CACHE_DIR=$(docker run --rm "$BUILD_IMAGE" bash -c 'yarn cache dir')
 
 docker run -v ${PWD}/.yarn_cache:$YARN_CACHE_DIR -i --rm $BUILD_IMAGE bash <<CMD
   jazelle ci --cwd $PROJECT
