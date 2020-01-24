480d8e20957ec4e9ae8d7243afb9185b51bf64ff
Albert Wu <wua@uber.com>
2020-01-24 10:37:59 -0800

    add check to noop cover

--- cover.sh
+++ cover.sh
@@ -17,6 +17,18 @@ util_install_yarn_cache_tarball $PROJECT
 YARN_CACHE_DIR=$(docker run --rm "$BUILD_IMAGE" bash -c 'yarn cache dir')
 
 docker run -v ${PWD}/.yarn_cache:$YARN_CACHE_DIR -i --rm $BUILD_IMAGE bash <<CMD
+  HAS_COVERAGE_SCRIPT=$(
+    jq -e '.scripts.cover' $PROJECT/package.json > /dev/null \
+    && echo true \
+    || echo false
+  )
+
+  # If no cover script in package.json, return immediately
+  if [ $HAS_COVERAGE_SCRIPT = false ]; then
+    echo "scripts.cover missing in $PROJECT/package.json. Exiting without calculating coverage data."
+    exit 0
+  fi
+
   jazelle ci --cwd $PROJECT
   jazelle yarn cover --cwd $PROJECT
 CMD
